3d28015410b04b172c2dd64c65e26144390305c4
Oybek <oybekberdiev715@gmail.com>
2023-08-17 18:46:50 +0500

    post get getone put delete

--- imtihon/src/app.js
+++ imtihon/src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const config = require("config");
 const fileUpload = require("express-fileupload");
-const swaggerUi = require("swagger-ui-express")
 
-const { swaggerSpec } = require("./swagger/options");
+
+
 const routes = require("./routes");
 
 const app = express();
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(fileUpload());
 app.use(express.static(process.cwd() + "/uploads"));
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
 
 app.use("/api", routes);
 
